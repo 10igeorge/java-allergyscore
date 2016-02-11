276411bb56ea4bd3454fc60d9669fc2858fa5d32
Anna Lawley and Isabelle George <10izzygeorge@gmail.com>
2016-02-11 11:13:30 -0800

    build front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,25 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/score", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/score.vtl");
+
+      String userScore = request.queryParams("userScore");
+      Integer integerUserScore = Integer.parseInt(userScore);
+      String allergyScore = allergyScore(integerUserScore);
+
+      model.put("allergies", allergyScore);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
 
